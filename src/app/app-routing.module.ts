253c9433939cfe66cb871d5987ec30898b23a784
manuel9716 <manuelvalencia9716@gmail.com>
2024-10-29 17:58:00 -0500

    configuracion botones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConfiguracionGeneralComponent } from './configuracion-general/configuracion-general.component';
+import { ParametrosGeneralesComponent } from './configuracion-general/parametros-generales/parametros-generales.component';
+import { IntermediarioFinancieroComponent } from './configuracion-general/intermediario-financiero/intermediario-financiero.component';
 
 const routes: Routes = [
   { path: 'configuracion-general', component: ConfiguracionGeneralComponent },
+  { path: 'parametros-generales', component: ParametrosGeneralesComponent },
+  { path: 'intermediario-financiero', component: IntermediarioFinancieroComponent },
   // Otras rutas...
 ];
 
